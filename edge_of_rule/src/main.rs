d944342fcaddd55bf1753879736792df414743b0
AHuangMeow <ahuangmeow@qq.com>
2026-01-20 11:18:46 +0800

    chore: add bevy rapier2d

--- edge_of_rule/src/main.rs
+++ edge_of_rule/src/main.rs
@@ -14,22 +14,24 @@ use crate::entities::EntitiesPlugin;
 use crate::ui::UiPlugin;
 use bevy::prelude::*;
 use bevy::window::WindowMode;
+use bevy_rapier2d::prelude::*;
 
 fn main() {
+    let default_plugin = DefaultPlugins
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "校园疾跑：规则边缘".to_string(),
+                mode: WindowMode::Fullscreen,
+                ..default()
+            }),
+            ..default()
+        })
+        .set(ImagePlugin::default_nearest());
+
     App::new()
         .insert_resource(Msaa::Off)
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "校园疾跑：规则边缘".to_string(),
-                        mode: WindowMode::Fullscreen,
-                        ..default()
-                    }),
-                    ..default()
-                })
-                .set(ImagePlugin::default_nearest()),
-        )
+        .add_plugins(default_plugin)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(CorePlugin)
         .add_plugins(AssetsPlugin)
         .add_plugins(EntitiesPlugin)
