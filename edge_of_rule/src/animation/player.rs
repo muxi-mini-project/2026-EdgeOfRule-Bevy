47d56ca3e38b6b6fd8f7cfc4b077994a164c06ef
AHuangMeow <ahuangmeow@qq.com>
2026-01-20 21:26:25 +0800

    feat: add sprint animation

--- edge_of_rule/src/animation/player.rs
+++ edge_of_rule/src/animation/player.rs
@@ -5,31 +5,48 @@ use crate::{assets::player::PlayerAssets, entities::player::Player};
 
 pub fn player_walk(
     time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
     mut commands: Commands,
     player_assets: Res<PlayerAssets>,
-    mut player: Query<(Entity, &Velocity, &mut Handle<Image>), With<Player>>,
+    mut player: Query<(Entity, &Velocity, &Player, &mut Handle<Image>)>,
 ) {
     const VELOCITY_THERESHOULD: f32 = 10.0;
 
-    for (entity, velocity, mut handle) in &mut player {
+    for (entity, velocity, player, mut handle) in &mut player {
         let vx = velocity.linvel.x;
         let step_out = ((time.elapsed_seconds() / 0.1) as i32) % 2 == 0;
+        let horizontal_pressed = keyboard_input.pressed(KeyCode::KeyA)
+            || keyboard_input.pressed(KeyCode::ArrowLeft)
+            || keyboard_input.pressed(KeyCode::KeyD)
+            || keyboard_input.pressed(KeyCode::ArrowRight);
+        let squat_pressed =
+            keyboard_input.pressed(KeyCode::KeyS) || keyboard_input.pressed(KeyCode::ArrowDown);
+        let shift_pressed = keyboard_input.pressed(KeyCode::ShiftLeft)
+            || keyboard_input.pressed(KeyCode::ShiftRight);
+        let sliding_input = squat_pressed && horizontal_pressed && !shift_pressed;
+        let show_dash_texture =
+            player.is_dashing && (shift_pressed || player.dash_direction.y != 0.0);
 
-        if vx.abs() > VELOCITY_THERESHOULD {
-            let new_texture = match (vx, step_out) {
+        let new_texture = if show_dash_texture {
+            Some(if vx >= 0.0 {
+                player_assets.side_squat_textures[0].clone()
+            } else {
+                player_assets.side_squat_textures[1].clone()
+            })
+        } else if squat_pressed && !sliding_input {
+            Some(player_assets.front_squat_texture.clone())
+        } else if vx.abs() > VELOCITY_THERESHOULD {
+            Some(match (vx, step_out) {
                 (vx, true) if vx > 0.0 => player_assets.side_textures[0].clone(),
                 (vx, false) if vx > 0.0 => player_assets.side_textures[1].clone(),
                 (_, true) => player_assets.side_textures[2].clone(),
                 (_, false) => player_assets.side_textures[3].clone(),
-            };
-
-            if *handle != new_texture {
-                commands.entity(entity).insert(new_texture.clone());
-                *handle = new_texture;
-            }
+            })
         } else {
-            let new_texture = player_assets.front_texture.clone();
+            Some(player_assets.front_texture.clone())
+        };
 
+        if let Some(new_texture) = new_texture {
             if *handle != new_texture {
                 commands.entity(entity).insert(new_texture.clone());
                 *handle = new_texture;
